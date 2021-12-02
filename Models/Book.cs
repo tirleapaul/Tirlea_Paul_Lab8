67bad7d5c5df8212cbcbb9c94d18eb1c10684823
Paul Sebastian Bogdan Tirlea <Tirlea.Al.Paul@student.utcluj.ro>
2021-12-02 15:27:48 +0200

    Am realizat Lab9.

--- Models/Book.cs
+++ Models/Book.cs
@@ -11,10 +11,20 @@ namespace Tirlea_Paul_Lab8.Models
     {
         public int ID { get; set; }
 
+        [Required, StringLength(150, MinimumLength = 3)]
+
         [Display(Name = "Book Title")]
         public string Title { get; set; }
+        [RegularExpression(@"^[A-Z][a-z]+\s[A-Z][a-z]+$", ErrorMessage = "Numele autorului trebuie sa fie de forma 'Prenume Nume'"), Required,
+StringLength(50, MinimumLength = 3)]
+        //^ marcheaza inceputul sirului de caractere
+        //[A-Z][a-z]+ prenumele -litera mare urmata de oricate litere mici
+        //\s spatiu
+        //[A-Z][a-z]+ numele- litera mare urmata de oricate litere mici
+        //$ marcheaza sfarsitul sirului de caractere
         public string Author { get; set; }
 
+        [Range(1, 300)]
         [Column(TypeName = "decimal(6, 2)")]
         public decimal Price { get; set; }
 
--- Pages/Books/Create.cshtml.cs
+++ Pages/Books/Create.cshtml.cs
@@ -10,7 +10,7 @@ using Tirlea_Paul_Lab8.Models;
 
 namespace Tirlea_Paul_Lab8.Pages.Books
 {
-    public class CreateModel : PageModel
+    public class CreateModel : BookCategoriesPageModel
     {
         private readonly Tirlea_Paul_Lab8.Data.Tirlea_Paul_Lab8Context _context;
 
@@ -22,24 +22,40 @@ namespace Tirlea_Paul_Lab8.Pages.Books
         public IActionResult OnGet()
         {
             ViewData["PublisherID"] = new SelectList(_context.Set<Publisher>(), "ID", "PublisherName");
+            var book = new Book();
+            book.BookCategories = new List<BookCategory>();
+            PopulateAssignedCategoryData(_context, book);
             return Page();
         }
-
         [BindProperty]
         public Book Book { get; set; }
-
-        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
-        public async Task<IActionResult> OnPostAsync()
+        public async Task<IActionResult> OnPostAsync(string[] selectedCategories)
         {
-            if (!ModelState.IsValid)
+            var newBook = new Book();
+            if (selectedCategories != null)
             {
-                return Page();
+                newBook.BookCategories = new List<BookCategory>();
+                foreach (var cat in selectedCategories)
+                {
+                    var catToAdd = new BookCategory
+                    {
+                        CategoryID = int.Parse(cat)
+                    };
+                    newBook.BookCategories.Add(catToAdd);
+                }
             }
-
-            _context.Book.Add(Book);
-            await _context.SaveChangesAsync();
-
-            return RedirectToPage("./Index");
+            if (await TryUpdateModelAsync<Book>(
+            newBook,
+            "Book",
+            i => i.Title, i => i.Author,
+            i => i.Price, i => i.PublishingDate, i => i.PublisherID))
+            {
+                _context.Book.Add(newBook);
+                await _context.SaveChangesAsync();
+                return RedirectToPage("./Index");
+            }
+            PopulateAssignedCategoryData(_context, newBook);
+            return Page();
         }
     }
 }
